980b30c0ca8222f771cc129fec1661648a67af35
Nordennavic <37419223+Nordennavic@users.noreply.github.com>
2020-02-13 23:11:59 +0500

    *Full-Added canvas. *Bug with pseudo-class :hover.

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import $ from 'jquery';
+import $ from 'jquery';
 
 class Canvas extends React.Component {
         constructor(props) {
@@ -7,88 +7,42 @@ class Canvas extends React.Component {
             this.state={painting : true};
             this.toPainting= this.toPainting.bind(this);
             this.toErasing = this.toErasing.bind(this);
+            this.cleaning = this.cleaning.bind(this);
+            this.brushStroke = this.brushStroke.bind(this);
         }
 
             toPainting() {
             if(this.state.painting !== true)
                 this.setState({painting : true});
-            this.render { return <button type='button' id='brush' onClick={this.toPainting} disabled>Brush</button>}
         }
             toErasing() {
-            if(this.state.painting === true) {
-                this.setState({painting: false});
-                return <button type='button' id='eraser' onClick={this.toErasing}   disabled>Eraser</button>
+                if (this.state.painting === true) {
+                    this.setState({painting: false});
+                }
             }
 
-        /*clean.onclick = function(){
-            $("#grid").children().css("background", "white");
+             cleaning(){
+                 $("#grid").children().css("background", "white");
+            }
 
-        }
-        brush.onclick = function(){
-            paint = true;
-            brush.disabled='disabled';
-            eraser.disabled='';
+        brushStroke(event){
+        this.state.painting ? event.target.style.background = 'black' : event.target.style.background = 'white';
         }
 
-        eraser.onclick = function(){
-            paint = false;
-            eraser.disabled='disabled';
-            brush.disabled='';
-        }
-
-        function Click(event){
-            let tr = event.target;
-            if(paint === true)
-                tr.style.background = 'black';
-            else tr.style.background = 'white';
-        }*/
-
-    }
     render() {
+        let cells = [];                               // Dynamic adding "cells of canvas"
+        for(let i = 0; i < 49; i++) {
+            cells.push(<div key={i.toString()} onClick={this.brushStroke} />);
+        }
         return (
             <div>
                 <div id='grid'>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                    <div></div>
+                    {cells}
                 </div>
-                <div classname='toolsPanel'>
-                    <button type='button' id='brush' onClick={this.toPainting} disabled>Brush</button>
-                    <button type='button' id='eraser' onClick={this.toErasing} >Eraser</button>
-                    <button type='button' id='clean'>Clean</button>
+                <div className='toolsPanel'>
+                    <button type='button' id='brush' onClick={this.toPainting} disabled={this.state.painting}>Brush</button>
+                    <button type='button' id='eraser' onClick={this.toErasing} disabled={!this.state.painting}>Eraser</button>
+                    <button type='button' id='clean' onClick={this.cleaning}>Clean</button>
                 </div>
             </div>)
                 }
